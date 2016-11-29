10e84fd4aec6452908df02aea9c4ab212155842a
zerkmani <johan_sari@hotmail.com>
2016-11-29 10:38:52 +0100

    Small fixes to studentlist&teacherlist. Added regex to course class and userviewmodel.

--- LexiconLMS/Models/Course.cs
+++ LexiconLMS/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace LexiconLMS.Models {
     {
         public int? Id { get; set; }
         public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh\\:mm}")]
         public DateTime StartDateTime { get; set; }
         public string Description { get; set; }
 
--- LexiconLMS/Models/UserViewModels.cs
+++ LexiconLMS/Models/UserViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,16 @@ namespace LexiconLMS.Models
     public class UserViewModels
     {
         public int Id { get; set; }
+
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
         public string Adress { get; set; }
         public string Email { get; set; }
+
+        [Display(Name = "Course name")]
         public string CourseName { get; set; }
     }
 }
